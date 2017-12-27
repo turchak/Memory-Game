ae734a6cc7f12e6f0cc445939510386cecafb643
turchak <turchaksv@gmail.com>
2017-12-28 00:39:47 +0200

    change style cards

--- app.js
+++ app.js
@@ -3,18 +3,16 @@ let startArr = ['2', '2', '3', '3', '4', '4', '5', '5', 'J', 'J', 'Q', 'Q', 'K',
 let shuffleArr = shuffle(startArr);
 let container = document.querySelector('.container')
 let cartTemplate = `<div class="card">
-<!-- front -->
-<div class="card__front"></div>
-<!-- back -->
-<div class="card__back">
-  <div class="card__back-top">
-    <div class="card__back-top-number"></div>
-    <div class="card__back-top-suit">♠</div>
+<div class="card-back"></div>
+<div class="card-front">
+  <div class="card-front-top">
+    <div class="card-front-top-number"></div>
+    <div class="card-front-top-suit">♠</div>
   </div>
-  <div class="card__back-mid">♠</div>
-  <div class="card__back-bottom">
-    <div class="card__back-bottom-number"></div>
-    <div class="card__back-bottom-suit">♠</div>
+  <div class="card-front-mid">♠</div>
+  <div class="card-front-bottom">
+    <div class="card-front-bottom-number"></div>
+    <div class="card-front-bottom-suit">♠</div>
   </div>
 </div>
 </div>`;
@@ -28,7 +26,7 @@ let cards = document.querySelectorAll('.card');
 //clear function for reset table
 function clear() {
     cards.forEach(card => {
-        card.classList.remove('card__open');
+        card.classList.remove('card-open');
     })
 }
 
@@ -44,14 +42,14 @@ function shuffle(a) {
 //set shuffle value to our cards
 cards.forEach(function (card, i) {
     cards[i].dataset.name = shuffleArr[i];
-    card.querySelector('.card__back-top-number').innerHTML = shuffleArr[i];
-    card.querySelector('.card__back-bottom-number').innerHTML = shuffleArr[i];
+    card.querySelector('.card-front-top-number').innerHTML = shuffleArr[i];
+    card.querySelector('.card-front-bottom-number').innerHTML = shuffleArr[i];
 })
 
 
 cards.forEach(card => {
     card.addEventListener('click', function () {
-        this.classList.add('card__open');
+        this.classList.add('card-open');
         checkArr.push(this.dataset.name);
 
         //check if our arr is full
@@ -59,7 +57,7 @@ cards.forEach(card => {
 
             //check if our card is the same
             if (checkArr[0] === checkArr[1]) {
-                let sameCards = document.querySelectorAll('.card__open');
+                let sameCards = document.querySelectorAll('.card-open');
                 setTimeout(function () {
                     sameCards.forEach(card => {
                         card.style.visibility = 'hidden';
@@ -73,7 +71,7 @@ cards.forEach(card => {
         if (checkArr.length > 2) {
             clear();
             checkArr = [];
-            this.classList.add('card__open');
+            this.classList.add('card-open');
             checkArr.push(this.dataset.name);
         }
     });

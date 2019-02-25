7032ebe9af7906fdb05230297dda431f958bd88e
turchak <turchaksv@gmail.com>
2019-02-25 02:15:13 +0200

    Print cards

--- src/assets/js/card.js
+++ src/assets/js/card.js
@@ -1,45 +1,44 @@
 class Card {
-    constructor(host, name) {
-        this.host = host;
+    constructor(name, index) {
+        this.host = document.createElement('div');
+        this.host.classList.add('card')
         this.name = name;
         this.isOpen = false;
-        this.handleClick = this.handleClick(this);
-        this.host.addEventListener('click', this.handleClick);
+        this.index = index;
+        this.element;
+
     }
   
     get element() {
-      this.host.innerHTML += this.render();
+      this.host.innerHTML = this.render();
     }
   
     get opened() {
       return this.isOpen;
     }
-
-    handleClick(ev) {
-        this.toggle();
-    }
   
     toggle() {
       this.isOpen = !this.isOpen;
+      this.element;
     }
   
     render() {
-        return `
-            <div class="card">
-                <div class="card-back"></div>
-                <div class="card-front">
-                <div class="card-front-top">
-                    <div class="card-front-top-number">${this.name}</div>
-                    <div class="card-front-top-suit">♠</div>
-                </div>
-                <div class="card-front-mid">♠</div>
-                <div class="card-front-bottom">
-                    <div class="card-front-bottom-number">${this.name}</div>
-                    <div class="card-front-bottom-suit">♠</div>
-                </div>
-                </div>
+        const string =  `
+            <div class="card-back" data-name=${this.name} data-index=${this.index}></div>
+            <div class="card-front" data-name=${this.name}>
+            <div class="card-front-top">
+                <div class="card-front-top-number">${this.name}</div>
+                <div class="card-front-top-suit">♠</div>
+            </div>
+            <div class="card-front-mid">♠</div>
+            <div class="card-front-bottom">
+                <div class="card-front-bottom-number">${this.name}</div>
+                <div class="card-front-bottom-suit">♠</div>
+            </div>
             </div>
         `
+        this.host.innerHTML = string;
+        return this.host;
     }
 }
 
--- src/assets/js/game.js
+++ src/assets/js/game.js
@@ -4,16 +4,40 @@ class Game {
     constructor(host) {
         this.set = this.shuffle(['2', '2', '3', '3', '4', '4', '5', '5', 'J', 'J', 'Q', 'Q', 'K', 'K', 'A', 'A']);
         this.host = host;
-        
+        this.buffer = [];
+        this.cards = [];
+        // this.init = this.init.bind(this);
         this.init();
     }
 
     init() {
         this.host.addEventListener('click', ev => {
-            ev.target.classList.add('active')
+            this.cards[ev.target.dataset.index].toggle()
+            // this.cards[]
+            // this.buffer.push(ev.target.dataset.name)
+            // console.dir(ev.target)
+            // this.checkBuffer()
         })
-        this.set.forEach(el => new Card(this.host, el).element)
+        this.set.forEach((el,index) => { 
+            this.cards.push(new Card(el, index))
+            console.log(this.cards)
+            const cards  = this.cards.reduce((acc, val) => {
+                acc.push(val.render());
+                return acc;
+            },[])
+            console.log(...cards);
+            this.host.append(...cards);
+        })
     };
+
+    checkBuffer() {
+        if(this.buffer.length === 2) {
+            if (this.buffer[0] === this.buffer[1]) {
+                return 
+            }
+        }
+        return false;
+    }
     
     shuffle(arr) {
         for (let i = arr.length - 1; i > 0; i--) {
